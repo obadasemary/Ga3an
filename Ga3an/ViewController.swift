d433a18a8a8194b121ba76627c7352bcfdbf51aa
Abdelrahman Mohamed <obada.semary@gmail.com>
2016-04-06 12:09:17 +0300

    Initial Commit

---
+++ Ga3an/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ga3an
+//
+//  Created by Abdelrahman Mohamed on 4/6/16.
+//  Copyright © 2016 Abdelrahman Mohamed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
