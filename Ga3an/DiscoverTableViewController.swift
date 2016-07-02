b76d3e81fbe306cd23c22feb57217be60ae6d519
Abdelrahman Mohamed <obada.semary@gmail.com>
2016-07-02 19:59:26 +0300

    3.0 Add Activity Indicator Add Lazy Loading Images

--- Ga3an/DiscoverTableViewController.swift
+++ Ga3an/DiscoverTableViewController.swift
@@ -11,6 +11,8 @@ import CloudKit
 
 class DiscoverTableViewController: UITableViewController {
     
+    @IBOutlet var spinner: UIActivityIndicatorView!
+    
     var restaurants: [CKRecord] = []
 
     override func viewDidLoad() {
@@ -22,36 +24,76 @@ class DiscoverTableViewController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
         
+        spinner.hidesWhenStopped = true
+        spinner.center = view.center
+        view.addSubview(spinner)
+        spinner.startAnimating()
+        
         getRecordsFromCloud()
     }
     
     func getRecordsFromCloud() {
         
-
+        // Remove existing records before refreshing
+        restaurants.removeAll()
+        tableView.reloadData()
+        
+        // Fetch data using convenience API
         let cloudContainer = CKContainer.defaultContainer()
         let publicDatabase = cloudContainer.publicCloudDatabase
         let predicate = NSPredicate(value: true)
         let query = CKQuery(recordType: "Restaurant", predicate: predicate)
         
-        publicDatabase.performQuery(query, inZoneWithID: nil) { (results, error) in
+        // Create the query operation with the query
+        let queryOperation = CKQueryOperation(query: query)
+        queryOperation.desiredKeys = ["name"]
+        queryOperation.queuePriority = .VeryHigh
+        queryOperation.resultsLimit = 50
+        queryOperation.recordFetchedBlock = { (record: CKRecord!) -> Void in
             
-            if error != nil {
-                print(error)
+            if let restaurantRecord = record {
+                self.restaurants.append(restaurantRecord)
+            }
+        }
+        
+        queryOperation.queryCompletionBlock = { (cursor: CKQueryCursor?, error: NSError?) -> Void in
+            
+            if (error != nil) {
+                print("Failed to get data from iCloud - \(error?.localizedDescription)")
                 return
             }
             
-            if let results = results {
-                
-//                print("Completed the download of Restaurant data")
-                
-                self.restaurants = results
-                
-                NSOperationQueue.mainQueue().addOperationWithBlock({ 
-                    
-                    self.tableView.reloadData()
-                })
-            }
+            print("Successfully retrieve the data from iCloud")
+            
+            NSOperationQueue.mainQueue().addOperationWithBlock({
+                self.spinner.stopAnimating()
+                self.tableView.reloadData()
+            })
         }
+        
+        // Execute the query
+        publicDatabase.addOperation(queryOperation)
+        
+//        
+//        publicDatabase.performQuery(query, inZoneWithID: nil) { (results, error) in
+//            
+//            if error != nil {
+//                print(error)
+//                return
+//            }
+//            
+//            if let results = results {
+//                
+////                print("Completed the download of Restaurant data")
+//                
+//                self.restaurants = results
+//                
+//                NSOperationQueue.mainQueue().addOperationWithBlock({ 
+//                    
+//                    self.tableView.reloadData()
+//                })
+//            }
+//        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -76,16 +118,35 @@ class DiscoverTableViewController: UITableViewController {
         
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
 
+        // Configure the cell...
         let restaurant = restaurants[indexPath.row]
-        
         cell.textLabel?.text = restaurant.objectForKey("name") as? String
-        
-        if let image = restaurant.objectForKey("image") {
+        // Set default image
+        cell.imageView?.image = UIImage(named: "photoalbum")
+        // Fetch Image from Cloud in background
+        let publicDatabase = CKContainer.defaultContainer().publicCloudDatabase
+        let fetchRecordsImageOperation = CKFetchRecordsOperation(recordIDs: [restaurant.recordID])
+        fetchRecordsImageOperation.desiredKeys = ["image"]
+        fetchRecordsImageOperation.queuePriority = .VeryHigh
+        fetchRecordsImageOperation.perRecordCompletionBlock = {(record:CKRecord?,
+            recordID:CKRecordID?, error:NSError?) -> Void in
+            if (error != nil) {
+                print("Failed to get restaurant image: \(error!.localizedDescription)")
+                return
+            }
             
-            let imageAsset = image as! CKAsset
-            cell.imageView?.image = UIImage(data: NSData(contentsOfURL: imageAsset.fileURL)!)
+            if let restaurantRecord = record {
+                
+                NSOperationQueue.mainQueue().addOperationWithBlock() {
+                    
+                    if let imageAsset = restaurantRecord.objectForKey("image") as? CKAsset {
+                        cell.imageView?.image = UIImage(data: NSData(contentsOfURL:
+                            imageAsset.fileURL)!)
+                    }
+                }
+            }
         }
-        
+        publicDatabase.addOperation(fetchRecordsImageOperation)
         return cell
     }
     
